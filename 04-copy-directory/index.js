779238140377d571da5bac3ff56aed97bf7ac9bd
UliaRa <uliaraeva0904@gmail.com>
2022-11-06 17:26:03 +0300

    feat: do forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+function copyDir() {
+    const fs = require('fs');
+    const path = require('node:path');
 
+    const pathToFirstFolder = path.join(__dirname, 'files');
+    const pathToNewFolder = path.join(__dirname, 'files-copy');
+
+    fs.mkdir(pathToNewFolder, (err) => {
+        if (err) {
+            if (!err.code == 'EEXIST') throw err;
+        }
+    });
+
+    fs.readdir(pathToFirstFolder, (err, files) => {
+        if (err) throw err;
+        else {
+            files.forEach(file =>
+                fs.copyFile(path.join(pathToFirstFolder, path.basename(file)), path.join(pathToNewFolder, path.basename(file)), (err) => {
+                  if (err) throw err;
+                }));            
+        }
+    });
+}
+
+copyDir();
