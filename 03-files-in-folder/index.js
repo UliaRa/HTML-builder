d1424c02de7446470887bf91b63ffc7c8a301932
UliaRa <uliaraeva0904@gmail.com>
2022-11-06 17:30:46 +0300

    fix: remove directories from list in third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,18 +8,27 @@ function getFIles() {
         if (err) throw err;
         else {
             for (let i = 0; i < files.length; i++) {
-                let ext = path.extname(files[i]);
-                let name = path.basename(files[i], ext);
-                ext = ext.toString().slice(1);
-                let sizeOfFile;
                 fs.stat(path.join(__dirname, 'secret-folder', files[i]), (err, stats) => {
                     if (err) {
-                        console.log(`${name} - ${ext} - ${undefined}`);
                         throw err;
                     }
                     else {
-                        sizeOfFile = stats.size;
-                        console.log(`${name} - ${ext} - ${sizeOfFile}`);
+                        if (!stats.isDirectory()) {
+                            let ext = path.extname(files[i]);
+                            let name = path.basename(files[i], ext);
+                            ext = ext.toString().slice(1);
+                            let sizeOfFile;
+                            fs.stat(path.join(__dirname, 'secret-folder', files[i]), (err, stats) => {
+                            if (err) {
+                                console.log(`${name} - ${ext} - ${undefined}`);
+                                throw err;
+                            }
+                            else {
+                                sizeOfFile = stats.size;
+                                console.log(`${name} - ${ext} - ${sizeOfFile}`);
+                            }
+                        })
+                        }
                     }
                 })
             }
